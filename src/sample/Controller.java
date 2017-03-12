0cf62a580da125ae0454bda4aa3d714c7389a79c
Tamir Moshiashvili <norblet82@gmail.com>
2017-03-12 17:06:00 +0200

    push pulkl

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,9 @@
 package sample;
 
 import javafx.event.ActionEvent;
-
 public class Controller {
 
-    public void printMsg(ActionEvent event){
-        System.out.println("You pressed me!");
+    public void printit(ActionEvent event){
+        System.out.println("helloooooo");
     }
 }
