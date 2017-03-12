f45e49644c0354922752ee7b30bbbd35c20f8914
Eden <edenshuker1997@gmail.com>
2017-03-12 17:02:43 +0200

    b

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
 
 public class Controller {
+    @FXML
+    private TextField send_feild;
 
     public void printMsg(ActionEvent event){
         System.out.println("You pressed me!");
     }
+
 }
