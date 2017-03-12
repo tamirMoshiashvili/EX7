1b2db3815b7115fac839bd172731d912a6843b58
Tamir Moshiashvili <norblet82@gmail.com>
2017-03-12 15:29:07 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void printMsg(ActionEvent event){
+        System.out.println("You pressed me!");
+    }
+}
