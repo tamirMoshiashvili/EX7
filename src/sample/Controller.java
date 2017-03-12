d0e2a9475019126967501643bd904695522080e3
Tamir Moshiashvili <norblet82@gmail.com>
2017-03-12 17:58:55 +0200

    progress

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,24 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
-    public void printit(ActionEvent event){
-        System.out.println("helloooooo");
+    @FXML
+    private TextField commandTxt;
+
+    @FXML
+    private TextField numTaxiTxt;
+
+    public void showCmnd(ActionEvent event){
+        System.out.println(commandTxt.getText());
+        commandTxt.setText("");
+    }
+
+    public void showNumTaxi(ActionEvent event){
+        System.out.println(numTaxiTxt.getText());
+        numTaxiTxt.setText("");
     }
 }
