be0f6b1a2be1bf981fd1d98bd47198a8a428d294
Eden <edenshuker1997@gmail.com>
2017-03-13 14:01:18 +0200

    Merge branch 'master' of /home/eden/IdeaProjects/gui with conflicts.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,9 +3,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.KeyEvent;
+
 
 public class Controller {
 
@@ -16,29 +19,24 @@ public class Controller {
     private TextField numTaxiTxt;
 
     @FXML
-    private GridPane grid;
+    private ImageView taxi;
+
+    @FXML
+    private VBox vbTaxi;
 
     public void showCmnd(ActionEvent event) {
         System.out.println(commandTxt.getText());
         commandTxt.setText("");
-        double width = (grid.getPrefWidth() - grid.getHgap() * 9) / 10;
-        double height = (grid.getPrefHeight() - grid.getVgap() * 9) / 10;
-        Image red = new Image(getClass().getResourceAsStream("resources/red_car.png"));
-        ImageView red_img = new ImageView(red);
-        red_img.setFitHeight(height);
-        red_img.setFitWidth(width);
-        grid.add(red_img, 0, 0);
     }
 
     public void showNumTaxi(ActionEvent event) {
         System.out.println(numTaxiTxt.getText());
         numTaxiTxt.setText("");
-        Image bluish = new Image(getClass().getResourceAsStream("resources/bluish_car.png"));
-        ImageView bluish_img = new ImageView(bluish);
-        double width = (grid.getPrefWidth() - grid.getHgap() * 9) / 10;
-        double height = (grid.getPrefHeight() - grid.getVgap() * 9) / 10;
-        bluish_img.setFitHeight(height);
-        bluish_img.setFitWidth(width);
-        grid.add(bluish_img, 3, 3);
     }
+    public void upTaxi(KeyEvent event) {
+        if (event.getKeyCode() == KeyCode.UP.hashCode())
+            System.out.println("hello");
+    }
+
+
 }
