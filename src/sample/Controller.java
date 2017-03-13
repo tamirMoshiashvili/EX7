68ad8df4b4158dca61c22d23a7f5aa544cc2b2fd
Tamir Moshiashvili <norblet82@gmail.com>
2017-03-13 14:03:53 +0200

    showing images on click

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -28,6 +28,7 @@ public class Controller {
         red_img.setFitHeight(height);
         red_img.setFitWidth(width);
         grid.add(red_img, 0, 0);
+
     }
 
     public void showNumTaxi(ActionEvent event) {
